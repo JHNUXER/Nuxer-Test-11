9acdc7d9e111a979ab62ed90fc72911952924d0f
John Kayne <johnhoganuke2@gmail.com>
2017-08-11 14:26:33 -0400

    Update BasicBlock.java

--- src/main/java/nuxertest/block/BasicBlock.java
+++ src/main/java/nuxertest/block/BasicBlock.java
@@ -1,3 +1,25 @@
 public class BasicBlock extends Block {
+  public BasicBlock(String txnm) {
+    this(txnm,Material.stone);
+  }
+  
+  public BasicBlock(String txnm,float hardness) {
+    this(txnm,Material.stone,hardness);
+  }
+  
+  public BasicBlock(String txnm,Material mat) {
+    this(txnm,mat,5f);
+  }
+  
+  public BasicBlock(String txnm,Material mat,float hardness) {
+    this(txnm,mat,hardness,hardness);
+  }
+  
+  public BasicBlock(String txnm,Material mat,float hardness,float resistance) {
+    super(mat);
+    setTextureName(txnm);
+    setHardness(hardness);
+    setResistance(resistance);
+  }
   
 }
